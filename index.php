42ee04de0158ab305701f51750b0e4e4166579ea
NeoG14 <nicolaplatajob@gmail.com>
2022-09-20 14:36:34 -0300

    comiteado

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php include 'includes/header.php'; ?>
 
     <main class="contenedor">
@@ -14,34 +13,5 @@
     </main>
 
     <?php include 'includes/footer.php'; ?>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurant</title>
-    <link rel="stylesheet" href="./styles/normalize.css">
-    <link rel="stylesheet" href="./styles/styles.css">
-</head>
-<body>
-    <nav class="navegacion">
-        <a href="index.php">Inicio</a>
-        <a href="cocina.php">Cocina</a>
-        <a href="bar.php">Bar-tender</a>
-        <a href="cajero.php">Cajero</a>
-    </nav>
-    <div class="formulario-mozo">
-        <form action="">
-            <label for="">Comida/Bebida</label>
-            <input type="text">
-            <button>Agregar a la lista</button>
-            <label for="">Comentarios:</label>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-        </form>
-    </div>
-    hola
->>>>>>> 4d97139c2ffab871ddeb4285144143f5b2e36d9c
 </body>
 </html>
