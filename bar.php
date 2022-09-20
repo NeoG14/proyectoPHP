c565ae0bcd0770d75fc309f87166b6ef31a3d444
Franco Garcia <francogarcia1331@gmail.com>
2022-09-19 23:38:05 -0300

    se agregan estilos y normalize

--- bar.php
+++ bar.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bar</title>
+    <link rel="stylesheet" href="./styles/normalize.css">
+    <link rel="stylesheet" href="./styles/styles.css">
 </head>
 <body>
-<nav>
+    <nav>
         <a href="index.php">Inicio</a>
         <a href="cocina.php">Cocina</a>
         <a href="bar.php">Bar-tender</a>
--- index.php
+++ index.php
@@ -5,15 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant</title>
+    <link rel="stylesheet" href="./styles/normalize.css">
+    <link rel="stylesheet" href="./styles/styles.css">
 </head>
 <body>
-    <nav>
+    <nav class="navegacion">
         <a href="index.php">Inicio</a>
         <a href="cocina.php">Cocina</a>
         <a href="bar.php">Bar-tender</a>
         <a href="cajero.php">Cajero</a>
     </nav>
-    <div>
+    <div class="formulario-mozo">
         <form action="">
             <label for="">Comida/Bebida</label>
             <input type="text">
